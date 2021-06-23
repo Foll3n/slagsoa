9fd44a391ab60db0e2d0eab3408ce57bad1203ac
root <>
2021-06-23 16:53:47 +0200

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ const routes: Routes = [
   { path: 'utilisateurs', component: UtilisateursComponent },
   { path: '',   redirectTo: '/connexion', pathMatch: 'full' },
   { path: '**', redirectTo: '/connexion' },
+
 ];
 
 @NgModule({
