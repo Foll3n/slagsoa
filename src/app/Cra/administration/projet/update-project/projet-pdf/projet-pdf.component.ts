743359eaa4ce5cbee9b337776c1aa22d24519082
AlexisDeGuibert <alexis.deguibert@gmail.com>
2021-08-11 17:50:57 +0200

    merge

--- src/app/Cra/administration/projet/update-project/projet-pdf/projet-pdf.component.ts
+++ src/app/Cra/administration/projet/update-project/projet-pdf/projet-pdf.component.ts
@@ -6,6 +6,7 @@ import {ProjetHttpDatabase} from '../../../../../configuration-http/ProjetHttpDa
 import {HttpClient} from '@angular/common/http';
 import {formatDate} from '@angular/common';
 import {Pdf} from '../../../../models/projet/Pdf';
+import {ListPdf} from '../../../../models/projet/ListPdf';
 @Component({
   selector: 'app-projet-pdf',
   templateUrl: './projet-pdf.component.html',
@@ -13,26 +14,43 @@ import {Pdf} from '../../../../models/projet/Pdf';
 })
 export class ProjetPdfComponent implements OnInit {
   @ViewChild('content', {static:false}) el!: ElementRef
-  firstDay!:string;
-  lastDay!:string;
+  firstDayString!:string;
+  lastDayString!:string;
+  firstDay!:Date;
+  lastDay!:Date;
   pdf!:Pdf;
+  pdfList:ListPdf[] = [];
 
   projet!: Projet;
   constructor(private route: ActivatedRoute, private httpClient: HttpClient) {
-    var date = new Date(), y = date.getFullYear(), m = date.getMonth();
-    this.firstDay = formatDate(new Date(y, m, 1),'yyyy-MM-dd','fr');
-    this.lastDay = formatDate(new Date(y, m + 1, 0),'yyyy-MM-dd','fr');
+
+
+
   }
 
   ngOnInit(): void {
     this.projet = this.route.snapshot.queryParams as Projet;
-    var date = new Date();
-    var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-    var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-    console.log(this.projet, this.firstDay,this.lastDay);
+    this.initCurrentMonth();
+    // this.getDaysInMonth();
     this.chargerProjet();
   }
-
+  initCurrentMonth(){
+    var date = new Date(), y = date.getFullYear(), m = date.getMonth();
+    this.firstDayString = formatDate(new Date(y, m, 1),'yyyy-MM-dd','fr');
+    this.lastDayString = formatDate(new Date(y, m + 1, 0),'yyyy-MM-dd','fr');
+    this.firstDay = new Date(y, m, 1);
+    this.lastDay = new Date(y, m + 1, 0);
+    this. getDaysInMonth();
+  }
+  getDaysInMonth() {
+    let first = new Date(this.firstDay);
+      while(first < this.lastDay){
+        let dateString = formatDate(first,'yyyy-MM-dd','fr');
+          this.pdfList.push(new ListPdf(dateString, this.projet.code_projet, '0'));
+          first.setDate(first.getDate()+1);
+      }
+      console.log("iciiii ",this.pdfList)
+  }
   makePdf(){
     console.log("projet ---> ",this.projet);
     let pdf = new jsPDF('p','pt','a4');
@@ -42,13 +60,15 @@ export class ProjetPdfComponent implements OnInit {
       }
     });
   }
+
   chargerProjet(){
     const projetHttp = new ProjetHttpDatabase(this.httpClient);
-    const response = projetHttp.getPdF(this.firstDay.toString(),this.lastDay.toString(),this.projet.code_projet,'99');
+    const response = projetHttp.getPdF(this.firstDayString,this.lastDayString,this.projet.code_projet,'99');
     response.subscribe(reponse => {
       if(reponse.status == 'OK'){
         console.log(reponse,'ppppppppppppppppppppppppppp');
         this.pdf = reponse.result;
+
       }
       else{
         console.log("Erreur de requete de base de données");
